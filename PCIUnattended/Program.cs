7fdb899ad2043ae2add1f72c9dd33561b4a88301
Paul Kilfoil <paulkilfoil@outlook.com>
2018-10-22 09:39:32 +0200

    Add files via upload

--- PCIUnattended/Program.cs
+++ PCIUnattended/Program.cs
@@ -10,18 +10,20 @@ namespace PCIUnattended
 		{
 			try
 			{
-				PCIBusiness.Tools.LogInfo("PCIUnattended.Main/5","Started ...",210);
+				PCIBusiness.Tools.LogInfo("PCIUnattended.Main/10","Launched",210);
 				
 				int    k;
 				int    rows     = 1;
 				byte   mode     = 0;
 				string provider = "";
+				string args     = "";
 				string argValue;
 
 				for ( k = 0 ; k < arg.Length ; k++ )
 				{
 					argValue = arg[k].ToUpper().Trim();
-					PCIBusiness.Tools.LogInfo("PCIUnattended.Main/10","Arg[" + k.ToString() + "] = " + argValue,210);
+					args     = args + argValue + " ";
+//					PCIBusiness.Tools.LogInfo("PCIUnattended.Main/10","Arg[" + k.ToString() + "] = " + argValue,210);
 
 					if ( argValue.StartsWith("MODE=") )
 						mode = System.Convert.ToByte(argValue.Substring(5));
@@ -31,16 +33,18 @@ namespace PCIUnattended
 						provider = argValue.Substring(9);
 				}
 
+				PCIBusiness.Tools.LogInfo("PCIUnattended.Main/20","Args=" + args.Trim(),210);
+
 				using (PCIBusiness.Payments payments = new PCIBusiness.Payments())
 				{
-					PCIBusiness.Tools.LogInfo("PCIUnattended.Main/15","Processing ...",210);
+					PCIBusiness.Tools.LogInfo("PCIUnattended.Main/30","Start processing, Bureau="+provider+", Mode="+mode.ToString()+", Rows="+rows.ToString(),210);
 					k = payments.ProcessCards(provider,mode,rows);
-					PCIBusiness.Tools.LogInfo("PCIUnattended.Main/20","Finished, k="+k.ToString() + ", " + (payments.CountSucceeded+payments.CountFailed).ToString() + " payment(s) completed : " + payments.CountSucceeded.ToString() + " succeeded, " + payments.CountFailed.ToString() + " failed",210);
+					PCIBusiness.Tools.LogInfo("PCIUnattended.Main/40","Finished, Return code="+k.ToString() + ", " + (payments.CountSucceeded+payments.CountFailed).ToString() + " payment(s) completed : " + payments.CountSucceeded.ToString() + " succeeded, " + payments.CountFailed.ToString() + " failed",210);
 				}
 			}
 			catch (Exception ex)
 			{
-				PCIBusiness.Tools.LogException("PCIUnattended.Main/25","",ex);
+				PCIBusiness.Tools.LogException("PCIUnattended.Main/90","",ex);
 			}
 		}
 	}
